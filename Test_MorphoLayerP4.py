e8bb650c49159d0838d40325a1961c47e600c73a
Penaud-Polge <100219108+Penaud-Polge@users.noreply.github.com>
2023-09-26 18:59:05 +0200

    Tests

--- Test_MorphoLayerP4.py
+++ Test_MorphoLayerP4.py
@@ -4,14 +4,27 @@ from skimage.io import imread
 
 # get image
 
+(x_train_init, y_train_init), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+Image = np.zeros((1, 48, 48, 1))
+
+Image[0,10:38,10:38,0] = x_train_init[5:6,:,:]
+Image = Image.astype('float32')/255
+
+plt.figure()
+plt.imshow(Image[0,:,:,0])
+
+"""
 Image = imread('spiral_196.png')
 Image = Image.astype('float32')/255.0
 Image = np.expand_dims(Image, 0)
+"""
 
-# Lifting Kernel 
 
-# Creating some lifting kernel
+# Lifting Structural Element
 
+# Creating some lifting SE
+
+"""
 kernel1 = np.zeros((3, 3, 3, 1)).astype('float32')
 kernel1[:,:,0,0] = np.eye(3)*10.0
 kernel1[:,0,1,0] = 10.0
@@ -25,7 +38,24 @@ kernel2[2,2,1,0] = 10.0
 kernel2[0,1,2,0] = 10.0
 kernel2 = kernel2 - 10
 
+for i in range(3):
+    print(kernel1[...,i,0])
+
+for i in range(3):
+    print(kernel2[...,i,0])
+
 filter = tf.concat([kernel1, kernel2], axis=-1)
+"""
+
+filter = np.random.rand(3,3,1,2)
+filter = (filter > 0.7)*10.0
+filter = filter.astype('float32') - 10
+
+for i in range(1):
+    print(filter[...,i,0])
+
+for i in range(1):
+    print(filter[...,i,1])
 
 # Lifting Layers on P4
 
@@ -83,28 +113,24 @@ print("scalarMaxTimesPlusClosed Lifting Shape : ", scalarMaxTimesPlusClosedLifti
 MaxTimesPlusDilationLifting = MaxTimesPlusDilationLiftingP4(num_filters=2, kernel_size = (3,3), padding='valid')
 MaxTimesPlusDilationLifting.build(Image.shape[1:])
 MaxTimesPlusDilationLifting.kernel = filter
-print("Times Kernel : ", MaxTimesPlusDilationLifting.timesKernel)
 MaxTimesPlusDilatedLifting = MaxTimesPlusDilationLifting(Image)
 print("MaxTimesPlusDilated Lifting Shape : ", MaxTimesPlusDilatedLifting.shape)
 
 MaxTimesPlusErosionLifting = MaxTimesPlusErosionLiftingP4(num_filters=2, kernel_size = (3,3), padding='valid')
 MaxTimesPlusErosionLifting.build(Image.shape[1:])
 MaxTimesPlusErosionLifting.kernel = filter
-print("Times Kernel : ", MaxTimesPlusErosionLifting.timesKernel)
 MaxTimesPlusErodedLifting = MaxTimesPlusErosionLifting(Image)
 print("MaxTimesPlusEroded Lifting Shape : ", MaxTimesPlusErodedLifting.shape)
 
 MaxTimesPlusOpeningLifting = MaxTimesPlusOpeningLiftingP4(num_filters=2, kernel_size = (3,3), padding='valid')
 MaxTimesPlusOpeningLifting.build(Image.shape[1:])
 MaxTimesPlusOpeningLifting.kernel = filter
-print("Times Kernel : ", MaxTimesPlusOpeningLifting.timesKernel)
 MaxTimesPlusOpenedLifting = MaxTimesPlusOpeningLifting(Image)
 print("MaxTimesPlusOpened Lifting Shape : ", MaxTimesPlusOpenedLifting.shape)
 
 MaxTimesPlusClosingLifting = MaxTimesPlusClosingLiftingP4(num_filters=2, kernel_size = (3,3), padding='valid')
 MaxTimesPlusClosingLifting.build(Image.shape[1:])
 MaxTimesPlusClosingLifting.kernel = filter
-print("Times Kernel : ", MaxTimesPlusClosingLifting.timesKernel)
 MaxTimesPlusClosedLifting = MaxTimesPlusClosingLifting(Image)
 print("MaxTimesPlusClosed Lifting Shape : ", MaxTimesPlusClosedLifting.shape)
 
@@ -114,75 +140,249 @@ print("MaxTimesPlusClosed Lifting Shape : ", MaxTimesPlusClosedLifting.shape)
 plt.figure()
 plt.suptitle('Dilation Lifting')
 for i in range(2):
-    plt.subplot(3, 2, 1 + i), plt.imshow(DilatedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 3 + i), plt.imshow(scalarMaxTimesPlusDilatedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 5 + i), plt.imshow(MaxTimesPlusDilatedLifting[0,0,:,:,i])
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(DilatedLifting[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Dilation Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusDilatedLifting[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Dilation Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusDilatedLifting[0,j,:,:,i])
+
 
 # Show Eroded Images
 
 plt.figure()
 plt.suptitle('Erosion Lifting')
 for i in range(2):
-    plt.subplot(3, 2, 1 + i), plt.imshow(ErodedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 3 + i), plt.imshow(scalarMaxTimesPlusErodedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 5 + i), plt.imshow(MaxTimesPlusErodedLifting[0,0,:,:,i])
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(ErodedLifting[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Erosion Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusErodedLifting[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Erosion Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusErodedLifting[0,j,:,:,i])
 
 # Show Opened Images
 
 plt.figure()
 plt.suptitle('Opening Lifting')
 for i in range(2):
-    plt.subplot(3, 2, 1 + i), plt.imshow(OpenedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 3 + i), plt.imshow(scalarMaxTimesPlusOpenedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 5 + i), plt.imshow(MaxTimesPlusOpenedLifting[0,0,:,:,i])
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(OpenedLifting[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Opening Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusOpenedLifting[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Opening Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusOpenedLifting[0,j,:,:,i])
 
 # Show Closed Images
 
 plt.figure()
 plt.suptitle('Closing Lifting')
 for i in range(2):
-    plt.subplot(3, 2, 1 + i), plt.imshow(ClosedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 3 + i), plt.imshow(scalarMaxTimesPlusClosedLifting[0,0,:,:,i])
-    plt.subplot(3, 2, 5 + i), plt.imshow(MaxTimesPlusClosedLifting[0,0,:,:,i])
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(ClosedLifting[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Closing Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusClosedLifting[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Closing Lifting')
+for i in range(2):
+    for j in range(4):
+        plt.subplot(2, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusClosedLifting[0,j,:,:,i])
 
 plt.show()
 
+# P4 Structural Elements
+
+# Creating some P4 Structural Elements
+
+filter = np.random.rand(3,3,3,2,4)
+filter = (filter > 0.7)*10.0
+filter = filter.astype('float32') - 10
+
+
+
 # Layers P4
 
+print(ErodedLifting.shape)
+
 Dilation = DilationP4(num_filters=4, kernel_size = (3,3))
-Dilation.build(Image.shape[1:])
+Dilation.build(ErodedLifting.shape[2:])
+Dilation.kernel = filter
+DilatedP4 = Dilation(ErodedLifting)
+print("DilatedP4 shape : ", DilatedP4.shape)
 
 Erosion = ErosionP4(num_filters=4, kernel_size = (3,3))
-Erosion.build(Image.shape[1:])
+Erosion.build(DilatedLifting.shape[2:])
+Erosion.kernel = filter
+ErodedP4 = Erosion(DilatedLifting)
+print("ErodedP4 shape : ", ErodedP4.shape)
 
 Opening = OpeningP4(num_filters=4, kernel_size = (3,3))
-Opening.build(Image.shape[1:])
+Opening.build(ClosedLifting.shape[2:])
+Opening.kernel = filter
+OpenedP4 = Opening(ClosedLifting)
+print("OpenedP4 shape : ", OpenedP4.shape)
 
 Closing = ClosingP4(num_filters=4, kernel_size = (3,3))
-Closing.build(Image.shape[1:])
+Closing.build(OpenedLifting.shape[2:])
+Closing.kernel = filter
+ClosedP4 = Closing(OpenedLifting)
+print("ClosedP4 shape : ", ClosedP4.shape)
 
 
 scalarMaxTimesPlusDilation = scalarMaxTimesPlusDilationP4(num_filters=4, kernel_size = (3,3))
-scalarMaxTimesPlusDilation.build(Image.shape[1:])
+scalarMaxTimesPlusDilation.build(scalarMaxTimesPlusErodedLifting.shape[1:])
+scalarMaxTimesPlusDilation.kernel = filter
+scalarMaxTimesPlusDilatedP4 = scalarMaxTimesPlusDilation(scalarMaxTimesPlusErodedLifting)
+print("scalarMaxTimesPlusDilatedP4 shape : ", scalarMaxTimesPlusDilatedP4.shape)
 
 scalarMaxTimesPlusErosion = scalarMaxTimesPlusErosionP4(num_filters=4, kernel_size = (3,3))
-scalarMaxTimesPlusErosion.build(Image.shape[1:])
+scalarMaxTimesPlusErosion.build(scalarMaxTimesPlusDilatedLifting.shape[1:])
+scalarMaxTimesPlusErosion.kernel = filter
+scalarMaxTimesPlusErodedP4 = scalarMaxTimesPlusErosion(scalarMaxTimesPlusDilatedLifting)
+print("scalarMaxTimesPlusErodedP4 shape : ", scalarMaxTimesPlusErodedP4.shape)
 
 scalarMaxTimesPlusOpening = scalarMaxTimesPlusOpeningP4(num_filters=4, kernel_size = (3,3))
-scalarMaxTimesPlusOpening.build(Image.shape[1:])
+scalarMaxTimesPlusOpening.build(scalarMaxTimesPlusClosedLifting.shape[1:])
+scalarMaxTimesPlusOpening.kernel = filter
+scalarMaxTimesPlusOpenedP4 = scalarMaxTimesPlusOpening(scalarMaxTimesPlusClosedLifting)
+print("scalarMaxTimesPlusOpenedP4 shape : ", scalarMaxTimesPlusOpenedP4.shape)
 
 scalarMaxTimesPlusClosing = scalarMaxTimesPlusClosingP4(num_filters=4, kernel_size = (3,3))
-scalarMaxTimesPlusClosing.build(Image.shape[1:])
-
+scalarMaxTimesPlusClosing.build(scalarMaxTimesPlusOpenedLifting.shape[1:])
+scalarMaxTimesPlusClosing.kernel = filter
+scalarMaxTimesPlusClosedP4 = scalarMaxTimesPlusClosing(scalarMaxTimesPlusOpenedLifting)
+print("scalarMaxTimesPlusClosedP4 shape : ", scalarMaxTimesPlusClosedP4.shape)
 
 MaxTimesPlusDilation = MaxTimesPlusDilationP4(num_filters=4, kernel_size = (3,3))
-MaxTimesPlusDilation.build(Image.shape[1:])
+MaxTimesPlusDilation.build(MaxTimesPlusErodedLifting.shape[1:])
+MaxTimesPlusDilation.kernel = filter
+MaxTimesPlusDilatedP4 = MaxTimesPlusDilation(MaxTimesPlusErodedLifting)
+print("MaxTimesPlusDilatedP4 shape : ", MaxTimesPlusDilatedP4.shape)
 
 MaxTimesPlusErosion = MaxTimesPlusErosionP4(num_filters=4, kernel_size = (3,3))
-MaxTimesPlusErosion.build(Image.shape[1:])
+MaxTimesPlusErosion.build(MaxTimesPlusDilatedLifting.shape[1:])
+MaxTimesPlusErosion.kernel = filter
+MaxTimesPlusErodedP4 = MaxTimesPlusErosion(MaxTimesPlusDilatedLifting)
+print("MaxTimesPlusErodedP4 shape : ", MaxTimesPlusErodedP4.shape)
 
 MaxTimesPlusOpening = MaxTimesPlusOpeningP4(num_filters=4, kernel_size = (3,3))
-MaxTimesPlusOpening.build(Image.shape[1:])
+MaxTimesPlusOpening.build(MaxTimesPlusClosedLifting.shape[1:])
+MaxTimesPlusOpening.kernel = filter
+MaxTimesPlusOpenedP4 = MaxTimesPlusOpening(MaxTimesPlusClosedLifting)
+print("MaxTimesPlusOpenedP4 shape : ", MaxTimesPlusOpenedP4.shape)
 
 MaxTimesPlusClosing = MaxTimesPlusClosingP4(num_filters=4, kernel_size = (3,3))
-MaxTimesPlusClosing.build(Image.shape[1:])
+MaxTimesPlusClosing.build(MaxTimesPlusOpenedLifting.shape[1:])
+MaxTimesPlusClosing.kernel = filter
+MaxTimesPlusClosedP4 = MaxTimesPlusClosing(MaxTimesPlusOpenedLifting)
+print("MaxTimesPlusClosedP4 shape : ", MaxTimesPlusClosedP4.shape)
+
+
+plt.figure()
+plt.suptitle('Dilation P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(DilatedP4[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Dilation P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusDilatedP4[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Dilation P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusDilatedP4[0,j,:,:,i])
+
+
+# Show Eroded Images
+
+plt.figure()
+plt.suptitle('Erosion P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(ErodedP4[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Erosion P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusErodedP4[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Erosion P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusErodedP4[0,j,:,:,i])
+
+# Show Opened Images
+
+plt.figure()
+plt.suptitle('Opening P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(OpenedP4[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Opening P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusOpenedP4[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Opening P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusOpenedP4[0,j,:,:,i])
+
+# Show Closed Images
+
+plt.figure()
+plt.suptitle('Closing P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(ClosedP4[0,j,:,:,i])
+
+plt.figure()
+plt.suptitle('scalarMaxTimesPlus Closing P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(scalarMaxTimesPlusClosedP4[0,j,:,:,i])
+    
+plt.figure()
+plt.suptitle('MaxTimesPlus Closing P4')
+for i in range(4):
+    for j in range(4):
+        plt.subplot(4, 4, 1 + 4*i + j), plt.imshow(MaxTimesPlusClosedP4[0,j,:,:,i])
+
+plt.show()
